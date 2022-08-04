e404e41e9512edc62bf44c229497278630bd89c7
steveBaca <smbaca99@gmail.com>
2022-08-04 06:22:53 -0400

    push

--- frontend/src/containers/Home.js
+++ frontend/src/containers/Home.js
@@ -3,10 +3,14 @@ import { Link } from 'react-router-dom';
 
 const Home = () => (
 
-    <div>
-
-        This is a test for the Home page
-     
+    <div className='container'>
+        <div class='jumbotron mt-5'>
+            <h1 class='display-4'>Welcome to Auth System!</h1>
+            <p class='lead'>This is an incredible authentication system with production level features!</p>
+            <hr class='my-4' />
+            <p>Click the Log In button</p>
+            <Link class='btn btn-primary btn-lg' to='/login' role='button'>Login</Link>
+        </div>
     </div>
 );
 
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,7 +1,8 @@
-import { configureStore, applyMiddleware } from 'redux';
+import { applyMiddleware } from 'redux';
+import { configureStore } from '@reduxjs/toolkit';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import thunk from 'redux-thunk';
-import rootReducer from './reducers';
+import rootReducer from './reducers/auth';
 
 const initialState = {};
 
